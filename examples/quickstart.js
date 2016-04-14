855eb64980084586362858da96d463097a06894e
Mayank Kumar <mayankkumar@urbanclap.com>
2016-04-14 17:06:18 +0530

    Modidifed sample code for search

--- examples/quickstart.js
+++ examples/quickstart.js
@@ -4,6 +4,8 @@
 // See https://wit.ai/l5t/Quickstart
 
 const Wit = require('../').Wit;
+var request = require('request');
+var searchText = '';
 
 const token = (() => {
   if (process.argv.length !== 3) {
@@ -36,16 +38,88 @@ const actions = {
     if (loc) {
       context.loc = loc;
     }
+
+    const search = firstEntityValue(entities, 'search_query');
+    if (search) {
+      context.search = search;
+    }
+
+    const localSearch = firstEntityValue(entities, 'local_search_query');
+    if (localSearch) {
+      context.search = localSearch;
+    }
+
     cb(context);
   },
   error: (sessionId, msg) => {
     console.log('Oops, I don\'t know what to do.');
   },
-  'fetch-weather': (context, cb) => {
-    // Here should go the api call, e.g.:
-    // context.forecast = apiCall(context.loc)
-    context.forecast = 'sunny';
+  saveSearchText: (context, cb) => {
+    console.log(context);
+    if (context.search) {
+      searchText = context.search;
+      console.log(searchText);
+      context.result = true;
+    }
     cb(context);
+  },
+  getSearchedService: (context, cb) => {
+    console.log(context);
+    var city = 'city_delhi_v2';
+    if (context.loc) {
+      if (context.loc.indexOf('mumbai') >= 0) {
+        city = 'city_mumbai_v2';
+      }
+
+      if (context.loc.indexOf('bangalore') >= 0) {
+        city = 'city_bangalore_v2';
+      }
+
+      if (context.loc.indexOf('hyderabad') >= 0) {
+        city = 'city_hyderabad_v2';
+      }
+
+      if (context.loc.indexOf('chennai') >= 0) {
+        city = 'city_chennai_v2';
+      }
+
+      if (context.loc.indexOf('pune') >= 0) {
+        city = 'city_pune_v2';
+      }
+
+      if (context.loc.indexOf('ahmedabad') >= 0) {
+        city = 'city_ahmedabad_v2';
+      }
+
+      if (context.loc.indexOf('delhi') ||
+        context.loc.indexOf('gurgaon') ||
+        context.loc.indexOf('faridabad') ||
+        context.loc.indexOf('noida') >= 0) {
+        city = 'city_delhi_v2';
+      }
+
+    }
+
+    if (context.loc && context.search) {
+      var api = 'https://www.urbanclap.com/api/v1/customercategories/search/category/' + city + '/' + context.search;
+      request(api, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          var data = JSON.parse(body).success.data;
+          const length = data.length;
+          context.result = {};
+          for (var i = 0; i < length; i++) {
+            var key = i.toString();
+            context.result[key] = 'Get ' + data[i].display_name + ' on Urbanclap. Click here: https://www.urbanclap.com/request/' + data[i].key_name;
+          }
+          cb(context);
+        }
+      });
+    }
+    else {
+      context.result = 'Sorry no pros found near your area';
+      cb(context);
+    }
+    
   },
 };
 
